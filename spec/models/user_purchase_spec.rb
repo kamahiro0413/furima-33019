6799dc8c3e5822c1ba6e81affe4db9fbd8231b86
kamahiro0413 <hiroki0413ga@yahoo.co.jp>
2021-01-08 12:03:55 +0900

    購入機能の単体テスト修正

--- spec/models/user_purchase_spec.rb
+++ spec/models/user_purchase_spec.rb
@@ -81,7 +81,19 @@ RSpec.describe UserPurchase, type: :model do
       @user_purchase.token = nil
       @user_purchase.valid?
       expect(@user_purchase.errors.full_messages).to include("Token can't be blank")
-    end   
+    end
+    
+    it 'user_idが空だと購入できない' do
+      @user_purchase.user_id = nil
+      @user_purchase.valid?
+      expect(@user_purchase.errors.full_messages).to include("User can't be blank")
+    end
+
+    it 'item_idが空だと購入できない' do
+      @user_purchase.item_id = nil
+      @user_purchase.valid?
+      expect(@user_purchase.errors.full_messages).to include("Item can't be blank")
+    end
    end
  end
 end
